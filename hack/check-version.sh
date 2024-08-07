beeed05b107b314bb9c145a2691441ebfa2aec45
ehearneredhat <ehearne@redhat.com>
2024-08-07 15:43:32 +0100

    amend script to use less conditions
    Signed-off-by: ehearneredhat <ehearne@redhat.com>

--- hack/check-version.sh
+++ hack/check-version.sh
@@ -6,9 +6,6 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 # Check if the branch is "main"
 if [[ "$branch" == "main" ]]; then
     version="latest"
-# Check if the branch starts with "v" followed by a number
-elif [[ "$branch" =~ ^v[0-9] ]]; then
-    version="${branch:1}"
 # Otherwise, use the branch name as the version
 else
     version="$branch"
